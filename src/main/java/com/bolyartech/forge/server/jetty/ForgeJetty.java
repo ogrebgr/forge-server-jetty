03f8aa3d177a21ef25a393a8f9047abff3b59333
Ognyan Bankov <ogibankov@gmail.com>
2016-12-12 18:15:30 +0200

    adding WebPage

--- src/main/java/com/bolyartech/forge/server/jetty/ForgeJetty.java
+++ src/main/java/com/bolyartech/forge/server/jetty/ForgeJetty.java
@@ -4,17 +4,18 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServlet;
 
 
 abstract public class ForgeJetty {
+    private final org.slf4j.Logger mLogger = LoggerFactory.getLogger(this.getClass());
     private Server mServer;
 
     public void start() {
         mServer = new Server(8080);
 
-        HandlerCollection handlerCollection = new HandlerCollection();
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         context.addServlet(new ServletHolder(createMainServlet()),"/*");
@@ -24,7 +25,12 @@ abstract public class ForgeJetty {
             mServer.start();
             mServer.join();
         } catch (Exception e) {
-            e.printStackTrace();
+            mLogger.error("Error starting the server: ", e);
+            try {
+                mServer.stop();
+            } catch (Exception e1) {
+                //suppress
+            }
         }
     }
 
