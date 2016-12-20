7bb200b1aa5e92f92321ef5f44cd7afeb2dab3c8
Ognyan Bankov <ogibankov@gmail.com>
2016-12-20 10:19:51 +0200

    scram login working

--- src/main/java/com/bolyartech/forge/server/jetty/ForgeJettyConfigurationLoader.java
+++ src/main/java/com/bolyartech/forge/server/jetty/ForgeJettyConfigurationLoader.java
@@ -2,8 +2,6 @@ package com.bolyartech.forge.server.jetty;
 
 import com.bolyartech.forge.server.config.ForgeConfigurationException;
 
-import java.io.IOException;
-
 
 public interface ForgeJettyConfigurationLoader {
     ForgeJettyConfiguration load(ClassLoader cl) throws ForgeConfigurationException;}
